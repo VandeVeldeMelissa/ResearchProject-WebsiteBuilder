c763b5fd946cfc4438cae79fbf522ad673175f37
Melissa Van de Velde <melissa.van.de.velde@student.howest.be>
2023-01-09 20:22:22 +0100

    Added layout and font theme

--- pwa/src/screens/WebsiteBuilder.vue
+++ pwa/src/screens/WebsiteBuilder.vue
@@ -1,18 +1,30 @@
 <template>
-	<v-stage :config="configKonva">
-		<v-layer>
-			<v-circle :config="configCircle"></v-circle>
-		</v-layer>
-	</v-stage>
+	<header class="h-14 border-b-0.5 border-neutral-400 font-poppins">
+		<h1 class="border-r-0.5 h-14 w-12 border-neutral-400">M.</h1>
+	</header>
+	<main class="flex w-screen font-poppins">
+		<aside class="w-12 border-r-0.5 border-neutral-400">
+			<h1>Side nav</h1>
+		</aside>
+		<v-stage :config="configKonva">
+			<v-layer>
+				<v-circle :config="configCircle"></v-circle>
+			</v-layer>
+		</v-stage>
+		<aside class="w-3/12 border-l-0.5 border-neutral-400">
+			<h1>Hehe</h1>
+		</aside>
+	</main>
 </template>
 
 <script lang="ts">
+import { ref } from 'vue'
 export default {
 	setup() {
-		const configKonva = {
-			width: 200,
-			height: 200,
-		}
+		const configKonva = ref({
+			width: window.innerWidth * 0.75 - 48,
+			height: window.innerHeight - 56,
+		})
 
 		const configCircle = {
 			x: 100,
@@ -22,6 +34,11 @@ export default {
 			stroke: 'black',
 			strokeWidth: 4,
 		}
+
+		window.addEventListener('resize', () => {
+			configKonva.value.width = window.innerWidth * 0.75 - 48
+			configKonva.value.height = window.innerHeight - 56
+		})
 
 		return {
 			configKonva,
--- pwa/vite.config.ts
+++ pwa/vite.config.ts
@@ -4,5 +4,10 @@ import Unocss from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Unocss({})]
+	plugins: [
+		vue(),
+		Unocss({
+			rules: [['font-poppins', { 'font-family': 'Poppins, Helvetica, sans-serif;' }]],
+		}),
+	],
 })
