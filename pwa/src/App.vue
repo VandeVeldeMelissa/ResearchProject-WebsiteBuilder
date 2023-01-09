8163e5758d17a31531c2f53dcde6c79dda54d8f8
Melissa Van de Velde <melissa.van.de.velde@student.howest.be>
2023-01-09 19:02:28 +0100

    Added lucide icons

--- pwa/src/App.vue
+++ pwa/src/App.vue
@@ -1,32 +1,3 @@
 <template>
-	<v-stage :config="configKonva">
-		<v-layer>
-			<v-circle :config="configCircle"></v-circle>
-		</v-layer>
-	</v-stage>
+	<router-view></router-view>
 </template>
-
-<script lang="ts">
-export default {
-	setup() {
-		const configKonva = {
-			width: 200,
-			height: 200,
-		}
-
-		const configCircle = {
-			x: 100,
-			y: 100,
-			radius: 70,
-			fill: 'red',
-			stroke: 'black',
-			strokeWidth: 4,
-		}
-
-		return {
-			configKonva,
-			configCircle,
-		}
-	},
-}
-</script>
--- pwa/src/main.ts
+++ pwa/src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 import '@unocss/reset/tailwind.css'
 import 'uno.css'
+import App from './App.vue'
 import VueKonva from 'vue-konva';
+import router from './bootstrap/router'
 
 const app = createApp(App);
+app.use(router)
 app.use(VueKonva);
 app.mount('#app');
