b51ce97042ad6d97142572e213df8dceaaa0cd86
Melissa Van de Velde <melissa.van.de.velde@student.howest.be>
2023-01-12 10:11:55 +0100

    Added border around canvas

--- pwa/src/screens/WebsiteBuilder.vue
+++ pwa/src/screens/WebsiteBuilder.vue
@@ -1,160 +1,168 @@
 <template>
-	<header
-		class="border-b-0.5 font-poppins h-18 flex justify-between border-neutral-300"
-	>
-		<div
-			class="border-r-0.5 h-18 flex w-14 items-center justify-center border-neutral-300 text-3xl font-bold"
+	<div class="bg-slate-100">
+		<header
+			class="border-b-0.5 font-poppins h-18 flex justify-between border-neutral-300 mb-8 bg-white"
 		>
-			<h1>M<span class="text-blue-600">.</span></h1>
-		</div>
-		<div class="flex items-center gap-x-4 p-4">
-			<div class="relative w-[16rem]">
-				<button
-					@click="toggleIsPageSelectorOpen"
-					class="ring- flex w-full items-center justify-between rounded bg-slate-100 p-2 hover:bg-blue-50 hover:text-blue-600 transition-colors"
-				>
-					<span>Page: {{ selectedPage }}</span>
-					<span class="grid h-5 w-5 place-content-center text-2xl"
-						><ChevronDown
-					/></span>
-				</button>
-
-				<ul
-					class="z-2 absolute mt-2 w-full rounded bg-white ring-1 ring-gray-300 drop-shadow-sm"
-					v-if="isPageSelectorOpen"
-				>
-					<li
-						class="cursor-pointer select-none p-2 hover:bg-blue-50"
-						@click="setPage('Home')"
-					>
-						Home
-					</li>
-					<li
-						class="cursor-pointer select-none p-2 hover:bg-blue-50"
-						@click="setPage('Contact')"
-					>
-						Contact
-					</li>
-					<li
-						class="flex cursor-pointer select-none items-center gap-x-2 p-2 hover:bg-blue-50"
-						@click="setPage('Add page')"
-					>
-						<Plus />
-						Add page
-					</li>
-				</ul>
+			<div
+				class="border-r-0.5 h-18 flex w-14 items-center justify-center border-neutral-300 text-3xl font-bold"
+			>
+				<h1>M<span class="text-blue-600">.</span></h1>
 			</div>
-			<button class="flex items-center gap-x-4 rounded bg-slate-100 px-5 py-2 hover:bg-blue-50 hover:text-blue-600 transition-colors">
-				<Eye />Preview
-			</button>
-		</div>
-		<div class="border-l-0.5 borderneutral-300 flex w-3/12 justify-between p-4">
-			<button
-				class="flex items-center gap-x-4 rounded bg-blue-600 px-5 py-1 text-white transition-colors hover:bg-blue-700"
-			>
-				<Globe />Publish
-			</button>
-			<button
-				class="flex items-center gap-x-4 bg-slate-100 px-5 py-1 transition-colors hover:bg-blue-50 hover:text-blue-600"
-			>
-				Melissa<User />
-			</button>
-		</div>
-	</header>
-	<main class="font-poppins flex bg-slate-100">
-		<aside class="border-r-0.5 w-14 border-neutral-300 bg-white p-3">
-			<button
-				class="rounded p-1 transition-colors hover:bg-blue-50 hover:text-blue-600"
-			>
-				<MousePointer2 />
-			</button>
-			<button
-				class="mt-5 rounded p-1 transition-colors hover:bg-blue-50 hover:text-blue-600"
-			>
-				<Type />
-			</button>
-			<button
-				class="mt-5 rounded p-1 transition-colors hover:bg-blue-50 hover:text-blue-600"
-			>
-				<Image />
-			</button>
-			<button
-				class="mt-5 rounded p-1 transition-colors hover:bg-blue-50 hover:text-blue-600"
-			>
-				<Spline />
-			</button>
-			<button
-				class="mt-5 rounded p-1 transition-colors hover:bg-blue-50 hover:text-blue-600"
-			>
-				<Square />
-			</button>
-			<button
-				class="mt-5 rounded p-1 transition-colors hover:bg-blue-50 hover:text-blue-600"
-			>
-				<Triangle />
-			</button>
-			<button
-				class="mt-5 rounded p-1 transition-colors hover:bg-blue-50 hover:text-blue-600"
-			>
-				<Circle />
-			</button>
-			<button
-				class="mt-5 rounded p-1 transition-colors hover:bg-blue-50 hover:text-blue-600"
-			>
-				<Hexagon />
-			</button>
-			<button
-				class="mt-5 rounded p-1 transition-colors hover:bg-blue-50 hover:text-blue-600"
-			>
-				<ArrowBigRight />
-			</button>
-		</aside>
-		<v-stage :config="configKonva">
-			<v-layer>
-				<v-circle :config="configCircle"></v-circle>
-			</v-layer>
-		</v-stage>
-		<aside class="border-l-0.5 w-3/12 border-neutral-300 bg-white p-4">
-			<!--3 pages: elements, blocks and templates -->
-			<div class="mb-4 flex justify-between rounded">
-				<button class="border-b-2 border-blue-600 px-4 py-1">Elements</button>
+			<div class="flex items-center gap-x-4 p-4">
+				<div class="relative w-[16rem]">
+					<button
+						@click="toggleIsPageSelectorOpen"
+						class="ring- flex w-full items-center justify-between rounded bg-slate-100 p-2 transition-colors hover:bg-blue-50 hover:text-blue-600"
+					>
+						<span>Page: {{ selectedPage }}</span>
+						<span class="grid h-5 w-5 place-content-center text-2xl"
+							><ChevronDown
+						/></span>
+					</button>
+	
+					<ul
+						class="z-2 absolute mt-2 w-full rounded bg-white ring-1 ring-gray-300 drop-shadow-sm"
+						v-if="isPageSelectorOpen"
+					>
+						<li
+							class="cursor-pointer select-none p-2 hover:bg-blue-50"
+							@click="setPage('Home')"
+						>
+							Home
+						</li>
+						<li
+							class="cursor-pointer select-none p-2 hover:bg-blue-50"
+							@click="setPage('Contact')"
+						>
+							Contact
+						</li>
+						<li
+							class="flex cursor-pointer select-none items-center gap-x-2 p-2 hover:bg-blue-50"
+							@click="setPage('Add page')"
+						>
+							<Plus />
+							Add page
+						</li>
+					</ul>
+				</div>
 				<button
-					class="px-4 py-1 text-neutral-600 transition-colors hover:text-blue-600"
+					class="flex items-center gap-x-4 rounded bg-slate-100 px-5 py-2 transition-colors hover:bg-blue-50 hover:text-blue-600"
 				>
-					Blocks
-				</button>
-				<button
-					class="px-4 py-1 text-neutral-600 transition-colors hover:text-blue-600"
-				>
-					Templates
+					<Eye />Preview
 				</button>
 			</div>
-			<button
-				class="flex w-full justify-between rounded bg-slate-100 p-4 transition-colors hover:bg-blue-50 hover:text-blue-600"
-			>
-				<div class="flex gap-x-2"><Plus />Add text</div>
-				<Type />
-			</button>
-			<button
-				class="mt-4 flex w-full justify-between rounded bg-slate-100 p-4 transition-colors hover:bg-blue-50 hover:text-blue-600"
-			>
-				<div class="flex gap-x-2"><Plus />Add image</div>
-				<Image />
-			</button>
-			<button
-				class="mt-4 flex w-full justify-between rounded bg-slate-100 p-4 transition-colors hover:bg-blue-50 hover:text-blue-600"
-			>
-				<div class="flex gap-x-2"><Plus />Add line</div>
-				<Spline />
-			</button>
-			<button
-				class="mt-4 flex w-full justify-between rounded bg-slate-100 p-4 transition-colors hover:bg-blue-50 hover:text-blue-600"
-			>
-				<div class="flex gap-x-2"><Plus />Add shape</div>
-				<Square />
-			</button>
-		</aside>
-	</main>
+			<div class="border-l-0.5 borderneutral-300 flex w-3/12 justify-between p-4">
+				<button
+					class="flex items-center gap-x-4 rounded bg-blue-600 px-5 py-1 text-white transition-colors hover:bg-blue-700"
+				>
+					<Globe />Publish
+				</button>
+				<button
+					class="flex items-center gap-x-4 bg-slate-100 px-5 py-1 transition-colors hover:bg-blue-50 hover:text-blue-600"
+				>
+					Melissa<User />
+				</button>
+			</div>
+		</header>
+		<main class="font-poppins flex bg-white">
+			<aside class="border-r-0.5 w-14 border-neutral-300 bg-white p-3 -mt-8">
+				<button
+					class="rounded p-1 transition-colors hover:bg-blue-50 hover:text-blue-600"
+				>
+					<MousePointer2 />
+				</button>
+				<button
+					class="mt-5 rounded p-1 transition-colors hover:bg-blue-50 hover:text-blue-600"
+				>
+					<Type />
+				</button>
+				<button
+					class="mt-5 rounded p-1 transition-colors hover:bg-blue-50 hover:text-blue-600"
+				>
+					<Image />
+				</button>
+				<button
+					class="mt-5 rounded p-1 transition-colors hover:bg-blue-50 hover:text-blue-600"
+				>
+					<Spline />
+				</button>
+				<button
+					class="mt-5 rounded p-1 transition-colors hover:bg-blue-50 hover:text-blue-600"
+				>
+					<Square />
+				</button>
+				<button
+					class="mt-5 rounded p-1 transition-colors hover:bg-blue-50 hover:text-blue-600"
+				>
+					<Triangle />
+				</button>
+				<button
+					class="mt-5 rounded p-1 transition-colors hover:bg-blue-50 hover:text-blue-600"
+				>
+					<Circle />
+				</button>
+				<button
+					class="mt-5 rounded p-1 transition-colors hover:bg-blue-50 hover:text-blue-600"
+				>
+					<Hexagon />
+				</button>
+				<button
+					class="mt-5 rounded p-1 transition-colors hover:bg-blue-50 hover:text-blue-600"
+				>
+					<ArrowBigRight />
+				</button>
+			</aside>
+			<div class="w-8 bg-slate-100"></div>
+			<!-- CANVAS WITH KONVA -->
+			<v-stage ref="stage" :config="configKonva">
+				<v-layer ref="layer">
+					<!--Add object here-->
+					<v-circle :config="configCircle"></v-circle>
+				</v-layer>
+			</v-stage>
+			<div class="w-8 bg-slate-100"></div>
+			<aside class="border-l-0.5 w-3/12 border-neutral-300 bg-white p-4 -mt-8">
+				<div class="mb-4 flex justify-between rounded">
+					<button class="border-b-2 border-blue-600 px-4 py-1">Elements</button>
+					<button
+						class="px-4 py-1 text-neutral-600 transition-colors hover:text-blue-600"
+					>
+						Blocks
+					</button>
+					<button
+						class="px-4 py-1 text-neutral-600 transition-colors hover:text-blue-600"
+					>
+						Templates
+					</button>
+				</div>
+				<button
+					class="flex w-full justify-between rounded bg-slate-100 p-4 transition-colors hover:bg-blue-50 hover:text-blue-600"
+					@click="addTextElementToCanvas"
+				>
+					<div class="flex gap-x-2"><Plus />Add text</div>
+					<Type />
+				</button>
+				<button
+					class="mt-4 flex w-full justify-between rounded bg-slate-100 p-4 transition-colors hover:bg-blue-50 hover:text-blue-600"
+				>
+					<div class="flex gap-x-2"><Plus />Add image</div>
+					<Image />
+				</button>
+				<button
+					class="mt-4 flex w-full justify-between rounded bg-slate-100 p-4 transition-colors hover:bg-blue-50 hover:text-blue-600"
+				>
+					<div class="flex gap-x-2"><Plus />Add line</div>
+					<Spline />
+				</button>
+				<button
+					class="mt-4 flex w-full justify-between rounded bg-slate-100 p-4 transition-colors hover:bg-blue-50 hover:text-blue-600"
+				>
+					<div class="flex gap-x-2"><Plus />Add shape</div>
+					<Square />
+				</button>
+			</aside>
+		</main>
+	</div>
 </template>
 
 <script lang="ts">
@@ -175,6 +183,11 @@ import {
 	Eye,
 	ChevronDown,
 } from 'lucide-vue-next'
+import { onMounted } from 'vue'
+import * as Konva from 'konva'
+import Layer from 'konva/lib/Layer'
+import Stage from 'konva/lib/Stage'
+import Text from 'konva/lib/shapes/Text'
 
 export default {
 	components: {
@@ -198,8 +211,8 @@ export default {
 		const selectedPage: Ref<string> = ref('Home')
 
 		const configKonva = ref({
-			width: window.innerWidth * 0.75 - 56,
-			height: window.innerHeight - 72,
+			width: window.innerWidth * 0.75 - 56 - 2*32,
+			height: window.innerHeight - 72 -32,
 		})
 
 		const configCircle = {
@@ -227,9 +240,18 @@ export default {
 			isPageSelectorOpen.value = !isPageSelectorOpen.value
 		}
 
-		watch(isPageSelectorOpen, (value) => {
-			console.log(isPageSelectorOpen.value)
-		})
+		const addTextElementToCanvas = () => {
+			// const text = new Text({
+			// 	x: configKonva.value.width / 2,
+			// 	y: configKonva.value.height / 2,
+			// 	text: 'Hello World',
+			// 	fontSize: 30,
+			// 	fontFamily: 'Calibri',
+			// 	fill: 'black',
+			// })
+			// layer.add(text)
+			// layer.draw()
+		}
 
 		return {
 			configKonva,
@@ -238,6 +260,7 @@ export default {
 			isPageSelectorOpen,
 			selectedPage,
 			toggleIsPageSelectorOpen,
+			addTextElementToCanvas,
 		}
 	},
 }
